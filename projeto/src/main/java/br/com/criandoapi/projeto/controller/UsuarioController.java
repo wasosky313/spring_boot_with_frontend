a6a93bb7ef785c39387771fa58d90c594d09ecdc
Andy Jimenez Reyes <wasosky313@gmail.com>
2023-04-30 20:24:26 -0300

    chore: change status code for responses

--- projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,27 +27,27 @@ public class UsuarioController {
 	private IUsuario dao;
 	
 	@GetMapping
-	public List<Usuario> listaUsuarios () {
-		return (List<Usuario>) dao.findAll();
+	public ResponseEntity<List<Usuario>> listaUsuarios () {
+		List<Usuario> lista = (List<Usuario>) dao.findAll();
+		return ResponseEntity.status(200).body(lista);
 	}
 	
 	@PostMapping
-	public Usuario criarUsuario(@RequestBody Usuario usuario) {
+	public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario usuario) {
 		Usuario usuarioNovo = dao.save(usuario);
-		return usuarioNovo;
+		return ResponseEntity.status(201).body(usuarioNovo) ;
 	}
 	
 	@PutMapping
-	public Usuario editarUsuario(@RequestBody Usuario usuario) {
+	public ResponseEntity<Usuario> editarUsuario(@RequestBody Usuario usuario) {
 		Usuario usuarioNovo = dao.save(usuario);
-		return usuarioNovo;
+		return ResponseEntity.status(201).body(usuarioNovo) ;
 	}
 	
 	@DeleteMapping("/{id}")
-	public Optional<Usuario> excluirUsuario (@PathVariable Integer id) {
-		Optional<Usuario> usuario = dao.findById(id);
+	public ResponseEntity<?> excluirUsuario (@PathVariable Integer id) { /* com <?> decimos que é um objeto genérico*/
 		dao.deleteById(id);
-		return usuario;
+		return ResponseEntity.status(204).build();  /* build para quando não tem corpo*/
 	}
 	
 }

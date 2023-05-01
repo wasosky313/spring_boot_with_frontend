b2ce7432853f6996fe0fbf3e6cc81295ed883462
Andy Jimenez Reyes <wasosky313@gmail.com>
2023-04-30 23:56:52 -0300

    chore: capture and handle errors

--- projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,19 +1,15 @@
 package br.com.criandoapi.projeto.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import br.com.criandoapi.projeto.service.UsuarioService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.criandoapi.projeto.model.Usuario;
 
@@ -58,5 +54,19 @@ public class UsuarioController {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 		}
 		return ResponseEntity.status(200).build();
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
 	}
 }
